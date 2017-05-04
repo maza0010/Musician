e4a8bcbc645ba30d0d9d833e3ca3ac9530f50cd5
vince <maza0010@algonquinlive.com>
2017-05-04 03:52:25 -0400

    readd2
    added for second updates

--- index.php
+++ index.php
@@ -6,18 +6,41 @@
         <title>Kibukila.ca</title>
         <link href="css/bootstrap.min.css" rel="stylesheet">
         <link rel="stylesheet" href="style.css" media="all">
+        <link rel="stylesheet" href="/assets/css/bootstrap.css?x32898">
+<link rel="stylesheet" href="/assets/css/librerie.css?x32898">
+<link rel="stylesheet" href="/assets/css/app.css?v=6&amp;x32898">
     </head>        
-<body>
-<div><?php include("includes/Header.php"); ?></div> 
-<div></div>
-<div><?php include("carousel.html") ?></div>
-</br>
-<div><?php include("includes/main_content.php"); ?></div>
-<div><?php include("includes/sidebar.php"); ?></div>
-<div><?php include("includes/footer.php"); ?></div>
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
-</body>
+    <body>
+        <div><?php include("includes/Header.php"); ?></div> 
+        <div></div>
+        <div><?php include("carousel.html") ?></div>
+        </br>
+        <div class="black-bg">
+            <div class="container clearfix">
+                <div class="grid-2 text-center">
+                    <a href="#"><img class="img" src="images/faus1.jpg" alt="Videos" width="30%" height="24%" /></a>
+                    <h3><a href="#">Music Videos</a> &rarr;</h3>
+                </div>
+                <div class="grid-2 text-center">
+                    <a href="#"><img class="img" src="images/faus3.JPG" alt="Albums" width="30%" height="24%" /></a>
+                    <h3><a href="#">Albums</a> &rarr;</h3>
+                </div>
+                <div class="grid-2 text-center">
+                    <a href="#"><img class="img" src="images/faus4.JPG" alt="Photos" width="30%" height="24%" /></a>
+                    <h3><a href="#">Photos</a> &rarr;</h3>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div><?php include("includes/main_content.php"); ?></div>
+        </div>
+        <div class="container">
+            <div><?php include("includes/sidebar.php"); ?></div>
+        </div>
+        <div><?php include("includes/footer.php"); ?></div>
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="js/bootstrap.min.js"></script>
+    </body>
 </html>
